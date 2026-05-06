8bf86dc9a62d4b742a0a0d6305e36f095c0045a0
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-05-06 20:49:05 +0300

    Initial commit

---
+++ src/main/java/com/buzincuemanuel/trainapp/TrainAppApplication.java
@@ -0,0 +1,13 @@
+package com.buzincuemanuel.trainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buzincuemanuel/trainapp/TrainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzincuemanuel.trainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
