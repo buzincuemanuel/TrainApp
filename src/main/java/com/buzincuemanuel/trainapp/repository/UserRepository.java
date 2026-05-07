af253e1549bfb73d7ca3418b6439d2df249b3bef
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-05-07 15:26:10 +0300

    feat: implement User Service and add findByEmail method in user repository

--- src/main/java/com/buzincuemanuel/trainapp/repository/UserRepository.java
+++ src/main/java/com/buzincuemanuel/trainapp/repository/UserRepository.java
@@ -4,5 +4,10 @@ import com.buzincuemanuel.trainapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/buzincuemanuel/trainapp/service/UserService.java
@@ -0,0 +1,41 @@
+package com.buzincuemanuel.trainapp.service;
+
+import com.buzincuemanuel.trainapp.model.User;
+import com.buzincuemanuel.trainapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
