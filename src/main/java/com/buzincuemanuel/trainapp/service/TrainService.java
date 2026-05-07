8b19ee7a4902f67b1cc9439cdc34ddf340effbcc
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-05-07 13:53:19 +0300

    feat: implement Train Service

---
+++ src/main/java/com/buzincuemanuel/trainapp/service/TrainService.java
@@ -0,0 +1,40 @@
+package com.buzincuemanuel.trainapp.service;
+
+
+import com.buzincuemanuel.trainapp.model.Train;
+import com.buzincuemanuel.trainapp.repository.TrainRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Data
+@RequiredArgsConstructor
+public class TrainService {
+
+    private final TrainRepository trainRepository;
+
+    public Train save(Train train){
+        return trainRepository.save(train);
+    }
+
+    public Train update(Long id, Train train) {
+        train.setId(id);
+        return trainRepository.save(train);
+    }
+
+    public void deleteById(Long id) {
+        trainRepository.deleteById(id);
+    }
+
+    public List<Train> findAll() {
+        return trainRepository.findAll();
+    }
+
+    public Optional<Train> findById(Long id) {
+        return trainRepository.findById(id);
+    }
+}
