be7662a4303b13730f3390e3d885bd42c165d553
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-05-10 11:07:28 +0300

    feat: add method to find schedules by multiple routes

--- src/main/java/com/buzincuemanuel/trainapp/repository/TrainScheduleRepository.java
+++ src/main/java/com/buzincuemanuel/trainapp/repository/TrainScheduleRepository.java
@@ -1,8 +1,14 @@
 package com.buzincuemanuel.trainapp.repository;
 
+import com.buzincuemanuel.trainapp.model.Route;
 import com.buzincuemanuel.trainapp.model.TrainSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TrainScheduleRepository extends JpaRepository<TrainSchedule, Long>{}
+public interface TrainScheduleRepository extends JpaRepository<TrainSchedule, Long>{
+
+    List<TrainSchedule> findByRouteIn(List<Route> routes);
+}
