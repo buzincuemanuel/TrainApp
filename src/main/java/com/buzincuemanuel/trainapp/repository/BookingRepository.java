8a64e95c5d10d906f81def5bad06481a7b907c70
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-05-10 12:57:59 +0300

    feat: add queries in BookingRepository to support overbooking prevention

--- src/main/java/com/buzincuemanuel/trainapp/repository/BookingRepository.java
+++ src/main/java/com/buzincuemanuel/trainapp/repository/BookingRepository.java
@@ -1,8 +1,22 @@
 package com.buzincuemanuel.trainapp.repository;
 
 import com.buzincuemanuel.trainapp.model.Booking;
+import com.buzincuemanuel.trainapp.model.TrainSchedule;
+import com.buzincuemanuel.trainapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByTrainSchedule(TrainSchedule trainSchedule);
+
+    List<Booking> findByUser(User user);
+
+    @Query("SELECT COALESCE(SUM(b.numberOfSeats), 0) FROM Booking b WHERE b.trainSchedule = :schedule")
+    Integer sumSeatsBookedForSchedule(@Param("schedule") TrainSchedule schedule);
+}
