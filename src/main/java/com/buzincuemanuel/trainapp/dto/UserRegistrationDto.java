cdf8d0a348f54dd617df4cbb4462e507147a098c
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-05-10 17:06:08 +0300

    feat: implement UserRegistrationDto

---
+++ src/main/java/com/buzincuemanuel/trainapp/dto/UserRegistrationDto.java
@@ -0,0 +1,10 @@
+package com.buzincuemanuel.trainapp.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+    private String name;
+    private String email;
+    private String password;
+}
